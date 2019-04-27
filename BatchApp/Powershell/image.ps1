a05ec38f9b6cfd65addfacb3f4d40de8db2ee4a9
azuregomez <edgome@microsoft.com>
2019-04-27 15:10:45 -0500

    app pack

--- BatchApp/Powershell/image.ps1
+++ BatchApp/Powershell/image.ps1
@@ -1,17 +1,16 @@
 ﻿# First: sysprep: https://docs.microsoft.com/en-us/azure/virtual-machines/windows/capture-image-resource
 # Sysprep directory C:\Windows\System32\Sysprep
-$vmName = "batchvm"
+$vmName = "dz01-vm"
 $rgName = "batch-rg"
 $location = "CentralUS"
 $snapshotName = "batchvmsnapshot"
 $imageName = "batchimage"
 Stop-AzVM -ResourceGroupName $rgName -Name $vmName -Force
 Set-AzVm -ResourceGroupName $rgName -Name $vmName -Generalized
-$vm = get-azvm -ResourceGroupName $resourceGroupName -Name $vmName
+$vm = get-azvm -ResourceGroupName $rgName -Name $vmName
 $snapshotcfg =  New-AzSnapshotConfig -SourceUri $vm.StorageProfile.OsDisk.ManagedDisk.Id -Location $location -CreateOption copy
-New-AzSnapshot -Snapshot $snapshotcfg -SnapshotName $snapshotName -ResourceGroupName $resourceGroupName
+New-AzSnapshot -Snapshot $snapshotcfg -SnapshotName $snapshotName -ResourceGroupName $rgName
 $snapshot = Get-AzSnapshot -ResourceGroupName $rgName -SnapshotName $snapshotName
 $imageConfig = New-AzImageConfig -Location $location
 $imageConfig = Set-AzImageOsDisk -Image $imageConfig -OsState Generalized -OsType Windows -SnapshotId $snapshot.Id
 New-AzImage -ImageName $imageName -ResourceGroupName $rgName -Image $imageConfig
-get-azstorageaccount
