ffcca83ad4e9008fd999f1dce438ad17f8792429
azuregomez <edgome@microsoft.com>
2019-04-27 15:14:27 -0500

    app pak

--- BatchApp/Powershell/image.ps1
+++ BatchApp/Powershell/image.ps1
@@ -7,9 +7,9 @@ $snapshotName = "batchvmsnapshot"
 $imageName = "batchimage"
 Stop-AzVM -ResourceGroupName $rgName -Name $vmName -Force
 Set-AzVm -ResourceGroupName $rgName -Name $vmName -Generalized
-$vm = get-azvm -ResourceGroupName $resourceGroupName -Name $vmName
+$vm = get-azvm -ResourceGroupName $rgName -Name $vmName
 $snapshotcfg =  New-AzSnapshotConfig -SourceUri $vm.StorageProfile.OsDisk.ManagedDisk.Id -Location $location -CreateOption copy
-New-AzSnapshot -Snapshot $snapshotcfg -SnapshotName $snapshotName -ResourceGroupName $resourceGroupName
+New-AzSnapshot -Snapshot $snapshotcfg -SnapshotName $snapshotName -ResourceGroupName $rgName
 $snapshot = Get-AzSnapshot -ResourceGroupName $rgName -SnapshotName $snapshotName
 $imageConfig = New-AzImageConfig -Location $location
 $imageConfig = Set-AzImageOsDisk -Image $imageConfig -OsState Generalized -OsType Windows -SnapshotId $snapshot.Id
